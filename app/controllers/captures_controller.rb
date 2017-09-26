b83c91da0ca797029708e938098f40a28092906b
Kyle Grund <me@kylegrund.com>
2017-09-26 14:24:36 -0700

    Capture id is now correctly pulled from parameters.

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -9,10 +9,10 @@ class CapturesController < ApplicationController
 
   def show
     @id = @user.id.to_s
-    @capture_id = :capture_id
-    @capture = Capture.get_captures_for_user(@user.uid).select { |cap| cap["LOCATION"] == @capture_id}.first
-    if @capture == nil
-      redirect_to root_url, :alert => 'Invalid capture location.'
-    end
+    @capture_id = params[:capture_id]
+    @capture = Capture.get_captures_for_user(@user.uid).select { |cap| cap["LOCATION"] == @capture_id }.first
+
+    # if capture not found in users' captures return to root with an error
+    redirect_to root_url, :alert => 'Invalid capture location.' if @capture.nil?
   end
 end
