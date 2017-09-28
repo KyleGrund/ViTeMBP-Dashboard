ed2df4a0eef431e059114097dca438c30b0eff63
Kyle Grund <me@kylegrund.com>
2017-09-27 18:14:01 -0700

    Video upload form properties are now stored in environment variables.

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -8,6 +8,13 @@ class CapturesController < ApplicationController
   end
 
   def show
+    # get upload parameters
+    @s3_ul_bucket = Rails.application.secrets.s3_ul_bucket
+    @s3_ul_success_base = Rails.application.secrets.s3_ul_success_base
+    @s3_ul_access_key = Rails.application.secrets.s3_ul_access_key
+    @s3_ul_policy_doc = Rails.application.secrets.s3_ul_policy_doc
+    @s3_ul_signature = Rails.application.secrets.s3_ul_signature
+
     @id = @user.id.to_s
     @capture_id = params[:capture_id]
     @capture = Capture.get_captures_for_user(@user.uid).select { |cap| cap["LOCATION"] == @capture_id }.first
