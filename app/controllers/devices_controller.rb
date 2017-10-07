b69fd65a2fce490a926a30f7e842050d8cdde4e5
Kyle Grund <me@kylegrund.com>
2017-10-06 19:25:02 -0700

    Added Nokogiri parsing for the config xml string.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -16,25 +16,33 @@ class DevicesController < ApplicationController
     device = Device.get_device_config(serial,@user.uid)
 
     if device.nil?
-      redirect_to '/' + @user.id.to_s + '/devices', :alert => "Unknown device."
+      redirect_to '/' + @user.id.to_s + '/devices', :alert => 'Unknown device.'
     end
 
     @device_config = device['CONFIG'] || 'No Configuration Saved.'
     @device_id = device['ID']
     @device_changes_pending = device['UPDATED'] || 'false'
+    parse_config @device_config
   end
 
   def adddevice
     serial = params[:devserial]
     success = Device.register_device(serial, @user.uid)
     if success
-      redirect_to "/" + @user.id.to_s + "/devices/register", :notice => "Device successfully added."
+      redirect_to '/' + @user.id.to_s + '/devices/register', :notice => 'Device successfully added.'
     else
       if Device.is_device_registered(serial)
-        redirect_to "/" + @user.id.to_s + "/devices/register", :alert => "Could not add device, device is already registered."
+        redirect_to '/' + @user.id.to_s + '/devices/register', :alert => 'Could not add device, device is already registered.'
       else
-        redirect_to "/" + @user.id.to_s + "/devices/register", :alert => "Could not add device, check serial number and try again."
+        redirect_to '/' + @user.id.to_s + '/devices/register', :alert => 'Could not add device, check serial number and try again.'
       end
     end
+  end
+
+  def parse_config(config)
+    xml_config = Nokogiri::XML(config)
+    @sampling_frequency = xml_config.at_xpath('/configuration/samplingfrequency').to_f
+    @sensor_names = xml_config.xpath('/configuration/sensornames/name')
+    @sensor_bindings = xml_config.xpath('/configuration/sensorbindings/sensorbinding')
   end
 end
