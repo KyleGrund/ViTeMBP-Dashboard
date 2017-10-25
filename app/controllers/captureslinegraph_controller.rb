4d9e9732906dcb76398d35aaa2f95f5080552282
Kyle Grund <me@kylegrund.com>
2017-10-24 20:17:33 -0700

    Added authentication to captureslinegraph controller.

--- app/controllers/captureslinegraph_controller.rb
+++ app/controllers/captureslinegraph_controller.rb
@@ -1,4 +1,7 @@
 class CaptureslinegraphController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user?
+
   def show_all_sensors
     @id = @user.id.to_s
     @capture_id = params[:capture_id]
